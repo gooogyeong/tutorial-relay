6b6f1c5708eb84de58258d435782adb92dbc110c
Minkyung Lee <minkyunglee@Minkyungs-Air.fritz.box>
2023-04-05 09:54:27 +0200

    feature: added query and fragments

--- newsfeed/src/components/PosterByline.tsx
+++ newsfeed/src/components/PosterByline.tsx
@@ -1,28 +1,48 @@
 import * as React from "react";
 import Image from "./Image";
+import { graphql } from "relay-runtime";
+import { useFragment } from "react-relay";
+import type { PosterBylineFragment$key } from "./__generated__/PosterBylineFragment.graphql";
+
+const PosterBylineFragment = graphql`
+  fragment PosterBylineFragment on Actor {
+      name
+      profilePicture {
+        url
+      }
+  }
+`;
 
 export type Props = {
-  poster: {
-    name: string;
-    profilePicture: {
-      url: string;
-    };
-  };
-};
+  poster: PosterBylineFragment$key
+}
+
+// export type Props = {
+//   poster: {
+//     name: string;
+//     profilePicture: {
+//       url: string;
+//     };
+//   };
+// };
 
 export default function PosterByline({ poster }: Props): React.ReactElement {
+  const data = useFragment(
+    PosterBylineFragment,
+    poster
+  )
   if (poster == null) {
     return null;
   }
   return (
     <div className="byline">
       <Image
-        image={poster.profilePicture}
+        image={data.profilePicture}
         width={60}
         height={60}
         className="byline__image"
       />
-      <div className="byline__name">{poster.name}</div>
+      <div className="byline__name">{data.name}</div>
     </div>
   );
 }
